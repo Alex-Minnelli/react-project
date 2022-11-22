c690faf1c554609d0e47b75a0cc6da695853d84e
Alex Minnelli <alexminnelli@gmail.com>
2022-11-22 14:53:25 -0500

    state and prop exercise

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import React, { Component } from "react";
+import './App.css';
+
+export default class App extends React.Component {
+
+  constructor(props){
+
+    super(props);
+
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      list: ['ready', 'set', 'GO'],
+      text: ''
+    };
+  }
+
+  onSubmit(event){
+    event.preventDefault();
+    this.setState({list: [...this.state.list, this.state.text] })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.text} onChange={(event) => this.setState({ text: event.target.value })} />
+          <button type='submit' >Add</button>
+        </form>
+        <ul>
+          {this.state.list.map((ele, index) => {
+            return <li key={index}>{ele}</li>
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
