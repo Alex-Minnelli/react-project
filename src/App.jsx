dfcc0c18d5dab3104bc0aa80ac75cd031549b6ec
Alex Minnelli <alexminnelli@gmail.com>
2022-11-22 20:37:49 -0500

    hooks exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import FilmsList from './components/filmsList'
 import './App.css';
 
-export default class App extends React.Component {
+export default function App(props){
 
-  constructor(props){
+  let [list, setList] = useState(['ready', 'set', 'GO']);
+  let [text, setText] = useState('');
 
-    super(props);
-
-    this.onSubmit = this.onSubmit.bind(this);
-
-    this.state = {
-      list: ['ready', 'set', 'GO'],
-      text: ''
-    };
-  }
-
-  onSubmit(event){
+  function onSubmit(event){
     event.preventDefault();
-    this.setState({list: [...this.state.list, this.state.text] })
+
+    let newList = [...list, text];
+    setList(newList);
+    setText('');
   }
 
-  render() {
-    return (
-      <div>
-        <h1>Hello World</h1>
-        <form onSubmit={this.onSubmit}>
-          <input type="text" value={this.state.text} onChange={(event) => this.setState({ text: event.target.value })} />
-          <button type='submit' >Add</button>
-        </form>
-        <ul>
-          {this.state.list.map((ele, index) => {
-            return <li key={index}>{ele}</li>
-          })}
-        </ul>
-        <FilmsList />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={(event) => setText(event.target.value)} />
+        <button type='submit' >Add</button>
+      </form>
+      <ul>
+        {list.map((ele, index) => {
+          return <li key={index}>{ele}</li>
+        })}
+      </ul>
+      <FilmsList />
+    </div>
+  );
 }
--- src/components/filmsList.jsx
+++ src/components/filmsList.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default class FilmsList extends Component{
+/*export default class FilmsList extends Component{
     constructor(props){
         super(props);
 
@@ -26,4 +26,19 @@ export default class FilmsList extends Component{
             {this.state.list.map(ele => <li key={ele.id}>{ele.title}</li>)}
         </ul>
     }
+}*/
+
+export default function FilmsList(props){
+    const [list, setList] = useState([]);
+
+    function getFilms(){
+        fetch('https://ghibliapi.herokuapp.com/films')
+        .then(res => res.json())
+        .then(films => setList(films))
+        .catch(err => console.error(err))
+    }
+
+    useEffect(() => {getFilms();}, []);
+
+    return <ul>{list.map(film => <li key={film.id}>{film.title}</li>)}</ul>
 }
